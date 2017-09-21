fe66659cced98910f7e596d1b3a65c68e0fcc235
lennang <32047239+lennang@users.noreply.github.com>
2017-09-21 12:34:55 +0800

    1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-var name = "Lenna";
+var student_1 = "Lenna";
+var student_2 = "Andrew";
+
+var welcome_msg = "Hey, Lets be friends.." + student_1 + "," + student_2;
+
+function greet(name) {
+    console.log ("Hello " + name);
+}
+
+greet ("Lenna");
+
+greet ("clarrisa")
+
+greet ("mum")
+
+function welcome(firstName, lastName) {
+    var msg = "Hello " + firstName + " " + lastName;
+    return msg;
+     }
+
+
+     console.log ( welcome ("Loo", "Lenna") );
