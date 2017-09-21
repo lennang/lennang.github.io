ca09052884365a29076eb0a0632fbe8769886a9f
lennang <32047239+lennang@users.noreply.github.com>
2017-09-21 13:57:44 +0800

    2

--- index.js
+++ index.js
@@ -20,3 +20,22 @@ function welcome(firstName, lastName) {
 
 
      console.log ( welcome ("Loo", "Lenna") );
+
+
+     var days = 5;
+     var fullTime = false;
+     // 1.1  Write an if statement to determine if lesson is full time.
+     //      Part time lesson consists of 8 days.
+     // 1.2  To create an if statement, write:
+     //      if(days < 8){ ...code to fill...  }
+     // 1.3  To changee fullTime to true, in the if statement within the { curly braces } write:
+     //      fullTime = true;
+     // 1.4  Print the value of fullTime in the console by writing after 1.2:
+     //      console.log(fullTime);
+     // WRITE THE CODE FOR STEP 1 BELOW THIS LINE:
+
+     if (days >8 ) {fullTime = true;
+         
+     }
+
+     console.log (fullTime);
